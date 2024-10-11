b9418ec9ec0428b4278ee73b358f65c285baf57c
martian422 <445971099@qq.com>
2024-10-11 16:02:33 +0800

    update

--- scripts/my_script/train_1.sh
+++ scripts/my_script/train_1.sh
@@ -10,18 +10,18 @@ export HF_ENDPOINT=https://hf-mirror.com
 
 ulimit -n 65536
 
-CUDA_VISIBLE_DEVICES=0 torchrun \
-    --nnodes=$WORLD_SIZE --node-rank=$RANK --nproc_per_node=1 \
-    --master_port=11451 \
+CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun \
+    --nnodes=$WORLD_SIZE --node-rank=$RANK --nproc_per_node=4 \
+    --master_port=11452 \
     main.py \
     model=1B \
     data=llamaGen \
-    data.dataset_path=/workspace/intern/liaomingxiang/ARG-MDM/data/dataset_small  \
+    data.dataset_path=/workspace/intern/liaomingxiang/ARG-MDM/data/dataset_v3_clean  \
     data.cache_dir=/workspace/intern/liaomingxiang/ARG-MDM/data/cache \
-    wandb.name=mdm-1B-llamaGen-70k \
-    data.size=70k \
+    wandb.name=mdm-1B-llamaGen-600k-m256 \
+    data.size=600k \
     parameterization=subs \
-    mask_vocab_size=128 \
+    mask_vocab_size=256 \
     model.length=256 \
     eval.compute_generative_perplexity=False \
     sampling.steps=1000 \
